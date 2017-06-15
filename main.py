8d0a83d28c74868c693e9b7ad0945d5d866011ee
vlomnebudet <vlomnebudet@gmail.com>
2017-06-16 01:49:39 +0500

    ADD: Добавлен таймер

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from maket import *
+from PyQt5.QtCore import QTimer
 
 
 class MainWindow:
+    schet = "1";
 
     def __init__(self):
         super().__init__()
-        self.MainWindow = QtWidgets.QMainWindow()
+        self.main_window = QtWidgets.QMainWindow()
         ui = Ui_MainWindow()
-        ui.setupUi(self.MainWindow)
-        self.MainWindow.show()
+        ui.setupUi(self.main_window)
+        self.main_window.show()
+
+    def local_button_handler(self):
+        self.main_window.setWindowTitle("Лошадь со скальпелем" + self.schet)
+        self.schet += "1"
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-    app.exec_()
+
+    timer = QTimer()
+    timer.timeout.connect(window.local_button_handler)
+    timer.start(1000)
+
+    sys.exit(app.exec_())
